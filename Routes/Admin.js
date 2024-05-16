589ab2e356cdaeec691bac6b6c5165537f16d020
Pratik07007 <s.dhimal006@gmail.com>
2024-05-16 23:06:55 +0545

    AdminregistrationCompleted

---
+++ Routes/Admin.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const adminCredentialsValidiation = require("../middleware/ZodValidiation");
+const { admin } = require("../Db");
+const app = express();
+
+app.use(express.json());
+
+app.post("/adminRegister", adminCredentialsValidiation, async (req, res) => {
+  const { email, password } = req.body;
+  const respone = await admin.findOne({ email });
+  if (respone) {
+    res.status(403).json({ msg: `Admin with email ${email} already exists` });
+  } else {
+    admin
+      .create({ email, password })
+      .then(() => res.status(200).json({ msg: "Admin Created Succesfully" }));
+  }
+});
+
+app.listen(3000);
